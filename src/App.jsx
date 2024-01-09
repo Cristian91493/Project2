066f7f178fa5f2f2828403a2460c899dd83755fd
Cristian <cristian91493@gmail.com>
2024-01-08 23:43:08 -0500

    added card search and text compenents

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,12 @@ function App() {
 const key = import.meta.env.VITE_KEY
 const apiUrl="https://api.openweathermap.org/data/2.5/weather?units=imperial&q="
   return (
+<div className="card">
+  <div className="search">
+    <input type="text" />
+  </div>
+</div>
 
-
-<p>Weather App</p>
   )
 }
 
