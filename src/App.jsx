c5860f9e70d4d341f79249daab7b603124ee1bcd
Cristian <cristian91493@gmail.com>
2024-01-08 23:19:37 -0500

    added api url

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 
 function App() {
-
-
+const key = import.meta.env.VITE_KEY
+const apiUrl="https://api.openweathermap.org/data/2.5/weather?units=imperial&q="
   return (
+
+
 <p>Weather App</p>
   )
 }
